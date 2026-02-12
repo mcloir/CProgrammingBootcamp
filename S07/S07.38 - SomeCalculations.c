3543057f904eefed004602fc6922b35d1ee73181
mcloir <gomesdaniel@outlook.com>
2026-02-11 23:23:55 -0300

    Correction of S07.38

--- S07/S07.38 - SomeCalculations.c
+++ S07/S07.38 - SomeCalculations.c
@@ -6,7 +6,7 @@ int main()
     printf("%d - %d = %d\n", 10, 20, 10-20);
     printf("%d * %d = %d\n", 10, 20, 10*20);
     printf("%d / %d = %d\n", 10, 20, 10/20); // Here, we have only the integer part of the quocient, which is 0.
-    printf("%d % %d = %d\n", 10, 3, 10%3); 
+    printf("%d %% %d = %d\n", 10, 3, 10%3);  // The % operator is used to calculate the remainder, i,e, the rest of the divission. We have to write %% to print the % symbol in the output.
 
 
 
